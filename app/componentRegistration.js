0f0cb9c398d5bbf61434643af5dba6328f5984ed
bmcbrayer@precisionlender.com <bmcbrayer@precisionlender.com>
2015-08-11 14:07:50 -0400

    Added some help

--- app/componentRegistration.js
+++ app/componentRegistration.js
@@ -1,10 +1,14 @@
-ko.components.register('action', {
-  viewModel: function() {
-    if (params.text === null) {
-      throw new Error("Cannot have action without text!");
-    }
+(function(ko) {
 
-    this.text = ko.observable(params.text)
-  },
-  template: '<span data-bind="text: text"></span>'
-});
+  ko.components.register('action', {
+    viewModel: function() {
+      if (params.text === null) {
+        throw new Error("Cannot have action without text!");
+      }
+
+      this.text = ko.observable(params.text)
+    },
+    template: '<span data-bind="text: text()"></span>'
+  });
+
+})(ko);
