eeed730d607d2e894378d72c3e0f40bd4470d6d6
bmcbrayer@precisionlender.com <bmcbrayer@precisionlender.com>
2015-08-12 11:16:16 -0400

    Added debug statements

--- app/components/action.js
+++ app/components/action.js
@@ -2,7 +2,7 @@
   function viewModel (params) {
     var cleanParams = (params == null ? {} : params);
     var me = this;
-
+    debugger;
     me.id = cleanParams.id;
     me.text = cleanParams.text;
     me.activate = cleanParams.activate;
--- app/components/actions.js
+++ app/components/actions.js
@@ -1,7 +1,7 @@
 (function() {
   function viewModel (params) {
     var cleanParams = (params == null ? {} : params);
-
+    debugger;
     this.actions = cleanParams.actions;
     this.activateAction = cleanParams.activateAction;
 
