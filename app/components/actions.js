2cad4bf78271f11702289ad854cbf9c70c8d39f8
bmcbrayer@precisionlender.com <bmcbrayer@precisionlender.com>
2015-08-11 23:12:40 -0400

    More malarky

--- app/components/actions.js
+++ app/components/actions.js
@@ -1,10 +1,5 @@
-(function(ko, $) {
-
-  ko.components.register('actions', {
-    viewModel: function(params) {
+(function() {
+  return function(params) {
       console.log('foo yeah!');
-    },
-    template: $.get('/app/components/actions.tmpl.html')
-  });
-
-})(ko, jQuery);
+  }
+})();
