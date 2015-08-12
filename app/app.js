3465df1760f75426dce15b94b0c8f43fee1b2278
bmcbrayer@precisionlender.com <bmcbrayer@precisionlender.com>
2015-08-12 17:32:19 -0400

    Tweaking timings

--- app/app.js
+++ app/app.js
@@ -6,12 +6,22 @@ window.app = (function(me, ko, radio) {
     ko.applyBindings(me);
 
     // Create a few items
+    var creationSpeedMinimum = 200;
+
+    me.engine.onTick(function() {
+      creationSpeedMinimum *= .9;
+
+      if (creationSpeedMinimum < 50) {
+        creationSpeedMinimum = 50;
+      }      
+    }, 30);
+
     function createItemAtRandomTime() {
       me.engine.oneTick(function() {
           me.engine.createItem('ball');
 
           createItemAtRandomTime();
-      }, Math.floor(Math.random() * 180) + 60);
+      }, Math.floor(Math.random() * (creationSpeedMinimum * 1.5)) + creationSpeedMinimum);
     }
 
     createItemAtRandomTime();
