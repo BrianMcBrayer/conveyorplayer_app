811d2b0500882f50ca2ebd83fe19c88a7aa4cfc6
bmcbrayer@precisionlender.com <bmcbrayer@precisionlender.com>
2015-08-14 17:19:09 -0400

    Added notifications

---
+++ app/config.js
@@ -0,0 +1,12 @@
+(function(Messenger) {
+
+  Messenger.options = {
+    extraClasses: 'messenger-fixed messenger-on-top messenger-on-right',
+    theme: 'future',
+    messageDefaults: {
+      showCloseButton: false,
+      hideAfter: 2
+    }
+  };
+
+})(Messenger);
--- app/engine/engine-notifications.js
+++ app/engine/engine-notifications.js
@@ -1,19 +1,19 @@
-var app = (function(me) {
-  me.engine = (function(me) {
+var app = (function(me, Messenger) {
+  me.engine = (function(me, Messenger) {
 
     me.notifyPositive = notifyPositive;
     me.notifyNegative = notifyNegative;
 
     function notifyPositive(message) {
-
+      Messenger().success(message);
     }
 
     function notifyNegative(message) {
-
+      Messenger().error(message);
     }
 
     return me;
-  })(me.engine || {});
+  })(me.engine || {}, Messenger);
 
   return me;
-})(window.app);
+})(window.app, Messenger);
